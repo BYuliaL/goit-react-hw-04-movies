2196c01ae68fdac35a0befbccfddae99a3d94340
Yulia Bandura <yuliabandura@gmail.com>
2021-04-12 16:25:32 +0300

    add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
+
+export default App;
